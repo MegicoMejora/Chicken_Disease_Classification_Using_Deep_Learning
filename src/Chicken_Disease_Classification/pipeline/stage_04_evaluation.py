80a54ac3e17945480784048e6a7988df3f5b048b
Megico Mejora <megicobritto@gmail.com>
2023-07-21 13:01:02 +0100

    dvc added

--- src/Chicken_Disease_Classification/pipeline/stage_04_evaluation.py
+++ src/Chicken_Disease_Classification/pipeline/stage_04_evaluation.py
@@ -1,28 +1,31 @@
 from Chicken_Disease_Classification.config.configuration import ConfigurationManager
-from Chicken_Disease_Classification.components.data_ingestion import DataIngestion
+from Chicken_Disease_Classification.components.evaluation import Evaluation
 from Chicken_Disease_Classification import logger
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Evaluation stage"
 
-class DataIngestionTrainingPipeline:
+class EvaluationPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
-
-
+        val_config = config.get_valiadation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
 
 if __name__ =='__main__':
     try:
         logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = EvaluationPipeline()
         obj.main()
         logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx========x")
     except Exception as e:
         logger.exception(e)
         raise e
+
+    
+
+        
+        
